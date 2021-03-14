5c5679c1d3a7b65e187e4dc0d8bf00c07479e6f3
OsamaKhan220 <osama.ahmedkhan96@gmail.com>
2021-03-15 02:13:29 +0500

    calender and qr screen added

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Container, Text} from 'native-base';
+import {Alert, StyleSheet} from 'react-native';
+import {Container} from 'native-base';
+import {Calendar} from 'react-native-calendars';
 
 const CalendarScreen = () => {
   return (
     <Container style={styles.container}>
-      <Text>Calendar Screen</Text>
+      <Calendar
+        onDayPress={(day) => {
+          Alert.alert('You sleceted', day.dateString);
+        }}
+        monthFormat={'MMMM yyyy'}
+        hideExtraDays={true}
+        enableSwipeMonths={true}
+      />
     </Container>
   );
 };
--- screens/QRScreen.js
+++ screens/QRScreen.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import {Container, Text} from 'native-base';
+import {RNCamera} from 'react-native-camera';
 
+const barcodeRecognized = (barcode) => {
+  console.log('barcode===>', barcode);
+};
 const QRScreen = () => {
   return (
     <Container style={styles.container}>
-      <Text>QR Screen</Text>
+      <RNCamera
+        style={styles.scanner}
+        onGoogleVisionBarcodesDetected={barcodeRecognized}></RNCamera>
     </Container>
   );
 };
 
 const styles = StyleSheet.create({
   container: {},
+  scanner: {
+    flex: 1,
+    width: '100%',
+  },
 });
 
 export default QRScreen;
